4fb09e0dba4801d21bdf8d4819c47519ec495b1c
polianams <pmrtins@gmail.com>
2023-11-01 20:45:24 -0300

    feat: Handle 'Recurso não encontrado' error in error middleware

--- src/controllers/moviesControllers.ts
+++ src/controllers/moviesControllers.ts
@@ -1,28 +1,21 @@
 import { Request, Response } from "express";
-import axios, { AxiosError } from "axios";
-import { NotFoundError } from "../helpers/errorHelp";
+import axios from "axios";
 
 const apiURL = "https://swapi.dev/api/";
 
 export const moviesControllers = async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    if (id) {
-      const getMovie = await axios.get(`${apiURL}films/${id}`);
-      const movieData = getMovie.data;
 
-      const modifiedMovieData = {
-        title: movieData.title,
-        episode_id: movieData.episode_id.toString(),
-      };
+  if (id) {
+    const getMovie = await axios.get(`${apiURL}films/${id}`);
+    const movieData = getMovie.data;
 
-      return res.status(200).json(modifiedMovieData);
-    }
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    if (axiosError.response?.status === 404) {
-      throw new NotFoundError("Movie not found");
-    }
+    const modifiedMovieData = {
+      title: movieData.title,
+      episode_id: movieData.episode_id.toString(),
+    };
+
+    return res.status(200).json(modifiedMovieData);
   }
 
   const getMovies = await axios.get(`${apiURL}films`);
--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { ApiError } from "../helpers/errorHelp";
+import { AxiosError } from "axios";
+import { ApiError, NotFoundError } from "../helpers/errorHelp";
 
 export const errorMiddleware = (
   error: Error & Partial<ApiError>,
@@ -7,8 +8,12 @@ export const errorMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
+  if (error instanceof AxiosError && error.response?.status === 404) {
+    throw new NotFoundError("Resource not found.");
+  }
+
   const statusCode = error.statusCode ?? 500;
-  const message = error.statusCode ? error.message : "Internal Server Error";
+  const message = error.statusCode ? error.message : "Internal Server Error.";
   console.log(error.message);
   return res.status(statusCode).json({ message });
 };
