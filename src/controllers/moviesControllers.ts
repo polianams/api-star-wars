1babc59f90bf9c8ad1249e2d4525be9f466f522d
polianams <pmrtins@gmail.com>
2023-11-01 20:13:40 -0300

    feat: Add episode_id to movie listing route in the API

--- src/controllers/moviesControllers.ts
+++ src/controllers/moviesControllers.ts
@@ -5,11 +5,29 @@ const apiURL = "https://swapi.dev/api/";
 
 export const moviesControllers = async (req: Request, res: Response) => {
   try {
-    const getMovies = await axios.get(`${apiURL}films`);
-    const moviesData = getMovies.data.results;
-    const titlesArray = moviesData.map((movie: any) => movie.title);
+    const { id } = req.params;
 
-    return res.json({ movies: titlesArray });
+    if (id) {
+      const getMovie = await axios.get(`${apiURL}films/${id}`);
+      const movieData = getMovie.data;
+      const modifiedMovieData = {
+        title: movieData.title,
+        episode_id: movieData.episode_id.toString(),
+      };
+
+      return res.status(200).json(modifiedMovieData);
+    }
+
+    const getMovies = await axios.get(`${apiURL}films`);
+    const movieData = getMovies.data.results;
+    const modifiedMovieData = movieData.map((movie: any) => {
+      return {
+        title: movie.title,
+        episode_id: movie.episode_id.toString(),
+      };
+    });
+
+    res.status(200).json(modifiedMovieData);
   } catch (error) {
     return res.status(500).json({ message: error });
   }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,5 +6,6 @@ const routes = Router();
 
 routes.get("/", rootRouteController);
 routes.get("/movies", moviesControllers);
+routes.get("/movies/:id", moviesControllers);
 
 export { routes };
