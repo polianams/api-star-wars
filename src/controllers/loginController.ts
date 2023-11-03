bd7d8f442d6c1571621c5368da09be8b157f4985
polianams <pmrtins@gmail.com>
2023-11-02 22:59:19 -0300

    Add login function with authentication token

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+import { passwordJwt } from "../config/authConfig";
+import { NotFoundError, UnauthorizedError } from "../helpers/errorHelp";
+
+const prisma = new PrismaClient();
+
+export const loginController = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const existingUsername = await prisma.users.findFirst({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!existingUsername) {
+    throw new NotFoundError("Username not found.");
+  }
+
+  const passwordMatch = await bcrypt.compare(
+    password,
+    existingUsername.password
+  );
+
+  if (!passwordMatch) {
+    throw new UnauthorizedError("Invalid username or password.");
+  }
+
+  const token = jwt.sign({ id: existingUsername.id }, passwordJwt, {
+    expiresIn: "6h",
+  });
+
+  const { password: _, ...userData } = existingUsername;
+
+  return res.status(200).json({
+    ...userData,
+    token,
+  });
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { moviesController } from "../controllers/moviesController";
 import { getCharacterController } from "../controllers/characterController";
 import { getPlanetController } from "../controllers/planetController";
 import { createUserController } from "../controllers/userController";
+import { loginController } from "../controllers/loginController";
 
 const routes = Router();
 
@@ -19,5 +20,7 @@ routes.get("/planet", getPlanetController);
 routes.get("/planet/:id", getPlanetController);
 
 routes.post("/user", createUserController);
+
+routes.post("/login", loginController);
 
 export { routes };
